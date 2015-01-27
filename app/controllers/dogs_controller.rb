e82e9bf34aa4974698da91a95678f8f6340ed59b
Chris D'Ambrosio <cdambrosio@gmail.com>
2015-01-27 15:38:13 -0800

    update controller / model specs for petfinder

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,6 +1,14 @@
 class DogsController < ApplicationController
+  @@dogs_service = DogsService
+
   def index
-    @dogs = Dog.search
+    @dogs = dogs_service.search
     render :json => @dogs
+  end
+
+  private
+
+  def dogs_service
+    @@dogs_service.new
   end
 end
--- spec/controllers/dogs_controller_spec.rb
+++ spec/controllers/dogs_controller_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
 describe DogsController, type: :controller do
+  before do
+    DogsController.class_variable_set(:@@dogs_service, FakeDogsService)
+  end
+
+  after do
+    DogsController.class_variable_set(:@@dogs_service, DogsService)
+  end
+
   describe 'GET #index' do
     it 'responds successfully with an HTTP 200 status code' do
-      body = %`{"status":"ok","messages":{"generalMessages":[],"recordMessages":[]},"foundRows":876,"data":{"1027693":{"animalID":"1027693","animalOrgID":"1034","animalName":"Karma","animalBreed":"Chocolate Labrador Retriever \/ American Staffordshire Terrier \/ Mixed (short coat)","animalLocation":"92172"}}}`
-      stub_request(:post, 'https://api.rescuegroups.org/http/json').
-        with(headers: { 'Content-Type' => 'application/json' }).
-        to_return(status: 200, body: body)
-
       get :index
       expect(response).to be_success
     end
+  end
+end
+
+class FakeDogsService
+  def search(options = {})
+    [{ name: 'Bit' }]
   end
 end
