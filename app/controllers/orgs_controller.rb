cacb3d4759ee4bc732508cc2ebe396c77812920e
Chris D'Ambrosio <cdambrosio@gmail.com>
2015-01-27 18:59:53 -0800

    remove orgs

--- app/controllers/orgs_controller.rb
+++
@@ -1,6 +0,0 @@
-class OrgsController < ApplicationController
-  def show
-    org = OrgsService.new.show params[:id]
-    render json: org.to_json
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   root to: 'pages#main'
   get '/dogs', to: 'dogs#index'
-  resources :orgs, only: [:show]
 end
